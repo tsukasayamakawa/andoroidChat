3ade99d033d0705e897889dd8d558274ec329985
kousukeex <kousukeex1997@gmail.com>
2019-06-17 15:31:11 +0900

    テキスト送信のテスト実装

--- AndroidChat/app/src/main/java/com/CT2/androidchat/MainActivity.java
+++ AndroidChat/app/src/main/java/com/CT2/androidchat/MainActivity.java
@@ -2,12 +2,55 @@ package com.CT2.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import me.himanshusoni.chatmessageview.ChatMessageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static CharSequence username = "";
+    static ArrayList<TextView> log = new ArrayList<>();
+    static ArrayList<ChatMessageView> speak = new ArrayList<>();
+    private static LinearLayout scroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nameButton = findViewById(R.id.nameDecisionButton);
+        Button sendButton = findViewById(R.id.sendMsgButton);
+        final EditText name = findViewById(R.id.nameEditText);
+        final EditText mes = findViewById(R.id.messageTextEdit);
+        scroll = findViewById(R.id.chatfuki);
+
+        //名前入力決定ボタン処理
+        nameButton.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View view){
+               username = name.getText();
+           }
+        });
+
+        //送信ボタン処理
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if (mes.getText().length() != 0) {
+                    scrollAddLog(mes.getText());
+                    speak.get(speak.size() - 1).addView(log.get(speak.size() - 1));
+                    scroll.addView(speak.get(speak.size() - 1));
+                }
+            }
+        });
+    }
+    public void scrollAddLog(CharSequence mes){
+        speak.add(new ChatMessageView(this));
+        log.add(new TextView(this));
+        log.get(log.size() - 1).setText(mes);
     }
 }
